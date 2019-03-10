a7e93a3edc7224ee574fea82e26eb5a929692147
Ren Liu <liuren@cc1st.com>
2019-03-10 10:36:21 +0800

    组件分离

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 export default class App extends Component {
   render() {
     return (
         <div className="container">
-          {'Hello React !!!'}
+          <div className="row">
+            <div className="col-xs-1 col-xs-offset-11">
+              <Header />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-1 col-xs-offset-11">
+              <h1>Hello !!</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-1 col-xs-offset-11">
+              <Home />
+            </div>
+          </div>
         </div>
     );
   }
